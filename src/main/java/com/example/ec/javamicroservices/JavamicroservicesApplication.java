c323434eee6781eb59d0d0ce02b843dd9a7df16f
yogeshdhawale <yogesh.dhawale+dev@gmail.com>
2023-09-18 10:50:21 +0530

    basic java spring project with microservices and devcontiner.

---
+++ src/main/java/com/example/ec/javamicroservices/JavamicroservicesApplication.java
@@ -0,0 +1,13 @@
+package com.example.ec.javamicroservices;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class JavamicroservicesApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(JavamicroservicesApplication.class, args);
+	}
+
+}
---
+++ src/test/java/com/example/ec/javamicroservices/JavamicroservicesApplicationTests.java
@@ -0,0 +1,13 @@
+package com.example.ec.javamicroservices;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class JavamicroservicesApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
